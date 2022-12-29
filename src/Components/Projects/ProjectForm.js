7e308e97d1ce369c70467522d534df51d9503f34
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2022-12-29 19:27:55 +0300

    fix

--- src/Components/Projects/ProjectForm.js
+++ src/Components/Projects/ProjectForm.js
@@ -121,7 +121,7 @@ const ProjectForm = ({ handleSubmit, control, reset ,onSubmit,errors}) => {
                   label="Project Assistant"
                 >
                   {users.map((user) => {
-                    return <MenuItem value= {user.username}>{user.firstname + "." + user.lastname}</MenuItem>
+                    return <MenuItem value= {user.username}>{user.firstname + " " + user.lastname}</MenuItem>
                   })}
                 </Select>
               </FormControl>
